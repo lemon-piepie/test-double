9be0c8660da289ddd208405a3f6d8de93a69d982
lemon-piepie <purplechilde@163.com>
2020-08-04 11:43:44 +0800

    finished without mock method on class.

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,14 +3,16 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-
+    MockDoorPanel mockDoorPanel = new MockDoorPanel();
     @BeforeEach
     public void setUp() {
+        mockDoorPanel.close();
     }
 
     @Test
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,19 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-
+    MockGradeService mockGradeService = new MockGradeService();
+    GradeService gradeService = new GradeService(mockGradeService);
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        GradeService gradeService = new GradeService(new MockGradeService());
+        double result = gradeService.calculateAverageGrades(1L);
+        Assertions.assertEquals(90.0, result);
     }
 }
