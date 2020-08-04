8aee5a142bafcd8eb106f0f3ce6a4c50a502bbaf
lemon-piepie <purplechilde@163.com>
2020-08-04 13:30:45 +0800

    finished exercise two with mock and stub.

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -2,17 +2,33 @@ package stub;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-    MockGradeService mockGradeService = new MockGradeService();
-    GradeService gradeService = new GradeService(mockGradeService);
+    @Mock
+    private GradeSystem mockGradeSystem;
+    private GradeService gradeService;
+    @BeforeEach
+    public void init(){
+        gradeService = new GradeService(mockGradeSystem);
+    }
+
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        GradeService gradeService = new GradeService(new MockGradeService());
+        when(mockGradeSystem.gradesFor(any())).thenReturn(Arrays.asList(90.0, 95.0, 85.0));
         double result = gradeService.calculateAverageGrades(1L);
         Assertions.assertEquals(90.0, result);
     }
