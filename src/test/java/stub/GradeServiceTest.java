66d0537e42e05dfbb4a6e687eb2c5cd13cc0b198
lemon-piepie <purplechilde@163.com>
2020-08-05 22:47:13 +0800

    finished stub

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,35 +1,31 @@
 package stub;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
-    * */
-    @Mock
-    private GradeSystem mockGradeSystem;
-    private GradeService gradeService;
-    @BeforeEach
-    public void init(){
-        gradeService = new GradeService(mockGradeSystem);
-    }
+     */
+
+    // @Mock
+    GradeSystem mockGradeSystem = Mockito.mock(GradeSystem.class);
+    GradeService gradeService = new GradeService(mockGradeSystem);
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        when(mockGradeSystem.gradesFor(any())).thenReturn(Arrays.asList(90.0, 95.0, 85.0));
-        double result = gradeService.calculateAverageGrades(1L);
-        Assertions.assertEquals(90.0, result);
+        when(mockGradeSystem.gradesFor(0L)).thenReturn(Arrays.asList(80.0, 90.0, 100.0));
+        double result = gradeService.calculateAverageGrades(0L);
+        assertEquals(90.0, result);
     }
 }
